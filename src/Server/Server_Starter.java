9d976be9c184576fd8ef043d221eeb40f77cb8c3
obiknobi101 <spacequazar83@googlemail.com>
2014-03-21 12:03:20 +0100

    StoryServer

---
+++ src/Server/Server_Starter.java
@@ -0,0 +1,25 @@
+package Server;
+import java.net.*;
+import java.io.*;
+
+public class Server_Starter {
+    public static void main(String[] args) throws IOException {
+
+    if (args.length != 1) {
+        System.err.println("Usage: java KKMultiServer <port number>");
+        System.exit(1);
+    }
+
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+        
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+            while (listening) {
+	            new Server_Thread(serverSocket.accept()).start();
+	        }
+	    } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
